10a896c74686cfdd0548076a4eebdaac404b4f6f
Joe Linogao <linogaoj@tcd.ie>
2023-06-23 19:11:01 +0100

    updated root doc

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # main FastAPI app
-from typing import Union
 from api.players import GetPlayers
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 
 app = FastAPI(
     title="vlr.gg player API",
     description="An Unofficial REST API for player information from [vlr.gg](https://www.vlr.gg/). "
-    "Inspired by the other unofficial API by [Rehkloos](https://github.com/Rehkloos) ",
+    "Inspired by the other unofficial API by [axsddlr](https://github.com/axsddlr/vlrggapi) ",
     version="1.0.0",
     
     # set the docs url to be the root URL 
@@ -15,17 +14,27 @@ app = FastAPI(
     redoc_url=None,
 )
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 def read_root():
-    return {"Hello": "World"}
+    return {"Service running": True }
+
+"""
+Give option to search by player name or by player ID
+Also, async to allow for multipler users to use the api (i think that's how that works)
+"""
 
 
-@app.get("/player/{player_name}")
-def read_item(player_name: str):
-    return {"item_name": player_name}
+# @app.get("/player/{player_name}")
+# async def read_item(player_name: str):
+#     return {"item_name": player_name}
 
-@app.get("/playerID/{player_id}")
-def read_item(player_id: int):
-    player_obj = GetPlayers.get_player_by_id(player_id)
-    return player_obj
+
+@app.get("/playerID/{player_id}", status_code=status.HTTP_200_OK)
+async def read_item(player_id: int, response: Response):
+    try:
+        player_obj = GetPlayers.get_player_by_id(player_id)
+        return player_obj
+    except Exception as e:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return "Not a valid player ID"
     

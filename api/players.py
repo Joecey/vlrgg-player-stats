270dbb8ec1e3b093c4723bf04a3efdf5892e29d1
Joe Linogao <linogaoj@tcd.ie>
2023-06-23 19:02:16 +0100

    added canada flag started boilerplate

--- api/players.py
+++ api/players.py
@@ -1,3 +1,8 @@
+"""
+Get details of player by either specifying team ID or brute forcing name search
+TODO Also give option to list all players?
+"""
+
 import httpx 
 from selectolax.parser import HTMLParser
 
@@ -9,17 +14,23 @@ class Player:
     index: int
     ign: str
     fullName: str
+    country: str
 
 def get_html(player_id):
         url = "https://www.vlr.gg/player/{}".format(player_id)
         resp = httpx.get(url)
         return HTMLParser(resp.text)
 
-def parse_player(html, id):
+def parse_player_id(html, id):
         new_player = Player(
             index = id,
             ign = remove_special(html.css_first("h1.wf-title").text()),
-            fullName = html.css_first("h2.player-real-name").text()
+            fullName = html.css_first("h2.player-real-name").text(),
+            country = remove_special(
+                remove_esc_seq(html.css("div.ge-text-light")[0].text())).lower()
+            # currentTeam = 
+            # twitter = 
+            # twitch = 
         )
         
         # print(new_player)
@@ -29,12 +40,27 @@ def parse_player(html, id):
 def remove_special(text):
     return ''.join(e for e in text if e.isalnum())
 
+# remove special sequences
+def remove_esc_seq(text):
+    escapes = ''.join([chr(char) for char in range(1, 32)])
+    translator = str.maketrans('', '', escapes)
+    return text.translate(translator)
+
 class GetPlayers:
     def get_player_by_id(player_id): 
         html = get_html(player_id)
-        return(parse_player(html, player_id))
-
+        return(parse_player_id(html, player_id))
     
+    def get_player_by_name(player_name):
+        """
+        Here, we can reiterate until we find the player name. Although,
+        this will literally take forvere
+        """
+        return ("test")
+
+# testing 
+html = get_html(9)  
+print(parse_player_id(html, 9))
 
     
 
---
+++ api/teams.py
@@ -0,0 +1,4 @@
+"""
+Get details of team by either specifying team ID or brute forcing name search
+TODO Also give option to list all teams?
+"""
